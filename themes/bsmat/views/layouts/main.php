ead04d9ccae9f46ffd39898347221a09e7107082
Renato Meneghini <rmeneghini@gmail.com>
2020-10-04 14:54:32 -0300

    ingreso por login, se saco el menu acerca de, y se agrego la validacion en entidad cuit + tipo

--- themes/bsmat/views/layouts/main.php
+++ themes/bsmat/views/layouts/main.php
@@ -35,7 +35,7 @@
 							'htmlOptions'=>array('class'=>'nav navbar-top-links navbar-right'),
 							'items'=>array(
 								array('label'=>'Inicio', 'url'=>array('/site/index')),
-								array('label'=>'Acerca de', 'url'=>array('/site/page', 'view'=>'about')),
+								//array('label'=>'Acerca de', 'url'=>array('/site/page', 'view'=>'about')),
 								array('label'=>'Contacto', 'url'=>array('/site/contact')),
 								//array('label'=>'Seguridad', 'url'=>array('/seguridad/'),'visible'=>Yii::app()->authManager->checkAccess('super',Yii::app()->user->id)),
 								// array('label'=>'Registrarse', 'url'=>array('/site/registro'), 'visible'=>Yii::app()->user->isGuest),
